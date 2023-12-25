ae1e7e52c3b2ac333433bc249a0c45c9da5ebf4e
NikBK <nikhilbalakrishna5@gmail.com>
2023-12-26 01:17:23 +0530

    navigation in match tabs css change

--- app/ui/matches/MatchTabs.tsx
+++ app/ui/matches/MatchTabs.tsx
@@ -22,12 +22,12 @@ const MatchTabs = () => {
     const pathname = usePathname();
 
     return (
-        <div className='grid grid-cols-3 gap-1 sm:gap-2 uppercase sticky -top-4 bg-[#f0f8ff]'>
+        <div className='grid grid-cols-3 uppercase sticky -top-4 bg-[#f0f8ff]'>
             {matchTabs.map((tab: { title: string, link: string }, index: number) =>
                 <Link
                     href={tab.link}
                     key={index}
-                    className={`${pathname == tab.link ? 'bg-indigo-500 text-white' : ''} py-2 px-2 sm:px-4 rounded-t-xl hover:bg-indigo-500 overflow-hidden`}
+                    className={`${pathname == tab.link ? 'bg-indigo-500 text-white' : ''} ${index == 0 ? 'rounded-s-md' : index == 2 ? 'rounded-e-md' : ''} py-2 px-2 sm:px-4 hover:bg-indigo-500 overflow-hidden`}
                 >
                     {tab.title}
                 </Link>
