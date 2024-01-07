ba19a0f0b02ef5a2eca75a96afbbf7856ea5f440
NikBK <nikhilbalakrishna5@gmail.com>
2024-01-08 00:38:59 +0530

    added admin auth

---
+++ app/admin/ui/AdminNavLinks.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { HomeIcon, PuzzlePieceIcon, CurrencyRupeeIcon, WalletIcon, UserGroupIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
+import { getBasePath } from '@/app/lib/utils';
+
+const links = [
+    { name: 'Home', href: '/admin', icon: HomeIcon },
+    { name: 'Matches', href: '/admin/matches', icon: PuzzlePieceIcon },
+    { name: 'Users', href: '/admin/users', icon: UserGroupIcon },
+    { name: 'Earnings', href: '/admin/earnings', icon: CurrencyRupeeIcon },
+    { name: 'Wallet', href: '/admin/wallet', icon: WalletIcon },
+];
+
+export default function AdminNavLinks() {
+    const basePath = getBasePath();
+
+    return (
+        <>
+            {links.map((link) => {
+                const LinkIcon = link.icon;
+                return (
+                    <Link
+                        key={link.name}
+                        href={link.href}
+                        className={`flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3 ${basePath === link.href ? 'bg-sky-100 text-blue-600' : ''}`}
+                    >
+                        <LinkIcon className="w-6" />
+                        <p className="hidden md:block">{link.name}</p>
+                    </Link>
+                );
+            })}
+        </>
+    );
+}
